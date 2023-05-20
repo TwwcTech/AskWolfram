85d0c9abcdcba0827a58ffee74f5aa5361fe801c
twwct <twwc.tech@gmail.com>
2023-05-20 11:55:38 -0600

    API variables added

---
+++ resources/statics.py
@@ -0,0 +1,8 @@
+
+class API:
+    # TODO: when using the APIs, must using (.format) to complete the API calls
+    # TODO: must replace spacese (" ") with plus signs (+) - input
+    SIMPLE = "http://api.wolframalpha.com/v1/simple?appid={}&i={}%3f"
+    SHORT_ANSWERS = "http://api.wolframalpha.com/v1/result?appid={}&i={}%3f"
+    SUMMARY_BOXES = "http://www.wolframalpha.com/summaryboxes/v1/query?appid={}&path={}%3f"
+    SPOKEN_RESULTS = "http://api.wolframalpha.com/v1/spoken?appid={}&i={}%3f"
