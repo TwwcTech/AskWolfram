0174fa045cff9c9a8664dfaf88671f362eb2aebd
twwct <twwc.tech@gmail.com>
2023-05-20 11:55:10 -0600

    wolfconnect class created

---
+++ backend/connectwolf.py
@@ -0,0 +1,17 @@
+from resources.statics import API
+
+class WolfConnect:
+    def __init__(self, query) -> None:
+        self.query = query
+    
+    def simple_query(self):
+        pass
+
+    def short_query(self):
+        pass
+
+    def sum_boxes(self):
+        pass
+
+    def spoken_query(self):
+        pass
