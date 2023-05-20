1162cedd3939a1995825b990514c919ac0565b7c
twwct <twwc.tech@gmail.com>
2023-05-20 13:37:00 -0600

    class complete

--- backend/connectwolf.py
+++ backend/connectwolf.py
@@ -1,17 +1,27 @@
 from resources.statics import API
+import urllib.parse
+import requests
+
 
 class WolfConnect:
-    def __init__(self, query) -> None:
-        self.query = query
-    
-    def simple_query(self):
-        pass
+    def __init__(self, appid: str, query: str) -> None:
+        self.query = query.strip()
+        self.appid = appid.strip()
 
     def short_query(self):
-        pass
-
-    def sum_boxes(self):
-        pass
+        response = requests.get(
+            url=API.SHORT_ANSWERS.format(
+                self.appid,
+                urllib.parse.quote_plus(self.query)
+            )
+        )
+        return response.text
 
     def spoken_query(self):
-        pass
+        reponse = requests.get(
+            url=API.SPOKEN_RESULTS.format(
+                self.appid,
+                urllib.parse.quote_plus(self.query)
+            )
+        )
+        return reponse.text
