ceb54c67f8265bd6490558bad8415e75e30e358d
twwct <twwc.tech@gmail.com>
2023-05-20 13:36:05 -0600

    almost complete, create flow and remove testing

--- askwolf.py
+++ askwolf.py
@@ -1,3 +1,12 @@
+from backend.connectwolf import WolfConnect
 
 if __name__ == "__main__":
-    pass
+    user_query = input("Query: ")
+
+    ask = WolfConnect(
+        appid="2HPKYW-33TYAJLGTY",
+        query=user_query
+    )
+
+    print("\n"*2, ask.short_query(), "\n"*2)
+    print("\n"*2, ask.spoken_query(), "\n"*2)
