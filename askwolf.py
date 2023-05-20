e3de327759ed2498859cdcb540593933c886aad8
twwct <twwc.tech@gmail.com>
2023-05-20 17:47:30 -0600

    format cleanse

--- askwolf.py
+++ askwolf.py
@@ -1,15 +1,4 @@
 from backend.connectwolf import WolfConnect
 
 if __name__ == "__main__":
-    user_query = input("\nQuery: ")
-
-    ask = WolfConnect(
-        appid="2HPKYW-33TYAJLGTY",
-        query=user_query
-    )
-
-    print("\n"*2)
-    # print(ask.short_query())
-    # print(ask.spoken_query())
-    print(ask.llm())
-    print("\n"*2)
+    pass
