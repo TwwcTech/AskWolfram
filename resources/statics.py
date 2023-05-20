973dcb22c8e544eb04f7f5c9c216f5b66d28fb95
twwct <twwc.tech@gmail.com>
2023-05-20 14:03:04 -0600

    LLM variable added

--- resources/statics.py
+++ resources/statics.py
@@ -1,3 +1,5 @@
 class API:
     SHORT_ANSWERS = "http://api.wolframalpha.com/v1/result?appid={}&i={}%3f"
     SPOKEN_RESULTS = "http://api.wolframalpha.com/v1/spoken?appid={}&i={}%3f"
+    LLM = "https://www.wolframalpha.com/api/v1/llm-api?input={}&appid={}"
+
