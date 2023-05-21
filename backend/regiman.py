0fcbf1c54d2c5a4f9fa9bf3f2ac37cb2567bd1c8
twwct <twwc.tech@gmail.com>
2023-05-21 11:02:52 -0600

    new file and class added

---
+++ backend/regiman.py
@@ -0,0 +1,18 @@
+import os as ops
+import winreg
+import re
+
+
+class RegiMan:
+    def __init__(self, regpath: str, regtype: str) -> None:
+        self.regpath = regpath
+        self.regtype = regtype
+
+    def enum_reg(self):
+        pass
+
+    def create_regkey(self):
+        pass
+
+    def read_regkey(self):
+        pass
