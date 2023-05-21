d794ff3ca81ba6d47146cc2e13520ecaf502162d
twwct <twwc.tech@gmail.com>
2023-05-20 23:34:03 -0600

    menus file created; class and methods completed

---
+++ backend/console_manager/menus.py
@@ -0,0 +1,21 @@
+from backend.console_manager import manageconsole as mc
+
+
+class MunuPicker():
+    def __init__(self, title1: str) -> None:
+        self.menutitle = mc.format_title(
+            dashcount=5,
+            titlename=title1.upper()
+        )
+
+    def show_mainmenu(self) -> str:
+        menu_options = {
+            "1": "Query",
+            "2": "Detailed Query",
+            "3": "Exit"
+        }
+        mc.space(1)
+        print(self.menutitle)
+        for options, menutype in menu_options.items():
+            print(f"{options}-{menutype}")
+        mc.space(1)
